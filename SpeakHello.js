1e2f52c9d8e016520b17095e844ef256540ad23d
larrinliu <156002499+larrinliu@users.noreply.github.com>
2024-02-01 13:37:18 -0500

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -6,7 +6,7 @@
       // the "speak" method and which you will expose to the global context
       // See Lecture 52, part 1
       var helloSpeaker = {};
-      helloSpeaker.speak = function (window){
+      helloSpeaker.speak = function (window) {
         console.log(speakWord + " " + name);
       }
     
