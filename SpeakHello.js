d84699f18825227bfccc1250484d49b622ac8189
larrinliu <156002499+larrinliu@users.noreply.github.com>
2024-02-01 14:48:13 -0500

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,11 +1,10 @@
 (function (window) {
       var helloSpeaker = {};
-      helloSpeaker.speak(name) = function (speakWord, name) {
+      helloSpeaker.speak = function (name) {
+        var speakWord = "Hello";
         console.log(speakWord + " " + name);
       }
   
-      var speakWord = "Hello";
-      
       window.helloSpeaker.speak(name) = helloSpeaker.speak(name);
 
 })(window);
